1da525fe668e96ef390287b8ea8bdb333817004e
holyminhlu <110122231@st.tvu.edu.vn>
2022-10-27 16:29:36 +0700

    BTTHUCHANH1.3

---
+++ BTTHUCHANH1.3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int tong(int a, int b );
+ int main()
+{
+  int a,b;
+  printf("\n NHAP a : ");
+  scanf("%d",&a);
+  printf("\n NHAP b : ");
+  scanf("%d",&b);
+  int tong=a+b;
+  printf("\n TONG : %d + %d = % d ", a,b,tong);
+  
+}
