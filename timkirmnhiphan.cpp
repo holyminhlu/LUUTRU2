0ae78adf7c351a7d63f7f5137f8c8ff70745d7ec
holyminhlu <116790648+holyminhlu@users.noreply.github.com>
2023-04-18 10:01:06 +0700

    Add files via upload

---
+++ timkirmnhiphan.cpp
@@ -0,0 +1,156 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+ 	int xuatmang( int a[10000], int n);
+ 	int nhapmang( int a[10000], int n);
+ 	//	int insertionsort( int a[10000], int n);
+ 	void sorted(int a[10000], int n);
+ 	int timkiem(int a[10000], int n, int tk);
+    //	void thuchientimkiem(int a[10000], int n);
+    int timkiemnhiphan( int a[10000], int n, int x);
+ 	
+ int main()
+ {
+ 	system(" color 70 ");
+ 	int a[10000];
+	int n, tk;
+	int x;
+	 printf("Nhap vao so luong phan tu: ");
+	 scanf("%d",&n);
+	 nhapmang(a,n);
+	 xuatmang(a,n);
+	//  printf("\nMang sau khi da sap xep (insertion sort): ");
+	//  insertionsort(a,n);
+	//  sorted(a,n);
+    //	printf("\nNhap vao gia tri can tim: ");
+    //	scanf("%d", &tk);
+	printf("\nNhap vao gia tri can tim: ");
+	scanf("%d", &tk);
+	int kq = timkiem(a, n, tk);
+	if(kq==-1)
+	{
+		printf("\n Khong tim thay!");
+	}
+	else
+	{
+		printf("\n Tim thay gia tri %d tai a[%d] !", tk,kq );
+	}	
+	//timkiem(a,n,tk);
+     sorted(a,n);
+     printf("\n Mang sau khi duoc sap xep: ");
+     	for(int i=0; i<n; i++)
+     		printf("\n a[%d] = %d",i,a[i]);
+     		printf("\n Nhap so can tim kiem nhi phan: ");
+     		scanf("%d",&x);
+     int midle = timkiemnhiphan(a,n,x);
+     if ( midle == -1)
+     	printf("\n Khong tim thay gia tri %d ",x);
+     	else
+     		printf("\nTim thay phan tu tai vi tri a[%d] ", midle);
+	 return 0;
+ }
+ 
+int nhapmang( int a[10000], int n)
+ {
+ 	for( int i=0; i<n; i++)
+ 	{
+ 		printf("\n\tNhap vao phan tu thu A[%d]: ",i);
+ 		scanf("%d", &a[i]);
+ 	}
+ }
+ int xuatmang( int a[10000] , int n)
+ 
+{
+	printf("Phan tu cua mang la: ");
+	for( int i=0; i<n; i++)
+	{
+		printf("\n\n A[%d]=%d", i, a[i]);
+	}
+}
+/* int insertionsort( int a[10000], int n)
+
+ {
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = a[i];
+       j = i-1;
+       while (j >= 0 && a[j] > key)
+       {
+           a[j+1] = a[j];
+           j = j-1;
+       }
+       a[j+1] = key;
+   }
+}
+void sorted(int a[10000], int n)
+{
+   int i;
+   for (i=0; i < n; i++)
+       printf("\t%d ", a[i]);
+   printf("\n\t");
+}
+ 
+ 	*/
+int timkiem(int a[10000], int n, int tk)
+{
+	for(int i=0; i<n; i++)
+	{
+		if(a[i]==tk)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+/* void thuchientimkiem(int a[10000], int n)  
+
+{
+	int tk;
+	printf("\nNhap vao gia tri can tim: ");
+	scanf("%d", &tk);
+	int kq = timkiem(a, n, tk);
+	if(kq==1)
+	{
+		printf("\n Tim thay gia tri %d tai a[%d] !", tk, a[i] );
+	}
+	else
+	{
+		printf("\n Khong tim thay!");
+	}	
+}	*/
+void sorted(int a[10000], int n)
+ 	{
+ 		int tam;
+ 		for( int i; i< n-1; i++)
+ 		{
+ 			for( int j=n-1; j>i; j--)
+ 			{
+ 				if( a[i]>a[j])
+ 				{
+ 					tam=a[i];
+ 					a[i]=a[j];
+ 					a[j]=tam;
+ 				}
+ 			}
+ 		}
+ 		
+ 	}
+int timkiemnhiphan( int a[10000], int n, int x)
+{
+	int left = 0; 
+	int right = n-1;
+	int midle;
+	while ( left <= right)
+	{
+		midle = (left + right)/2;
+		if ( a[midle] == x )
+		return midle;
+			if (x > a[midle])
+				left = midle +1;
+			else
+				right = midle -1;
+	}
+			 return -1;
+}
+			
